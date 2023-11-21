c2393b957f0bd6d4683576e7c8d89febc9330d33
KMarly D <44208106+Kemarly@users.noreply.github.com>
2023-11-21 11:53:40 -0500

    Initial commit

---
+++ app/src/main/java/com/example/kotlinartisan/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.example.kotlinartisan
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.ImageButton
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.kotlinartisan.ui.theme.KotlinArtisanTheme
+private lateinit var startButton: ImageButton
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_start_screen)
+
+        startButton = findViewById(R.id.startButton)
+        startButton.setOnClickListener { val intent = Intent(this@MainActivity, MainActivity::class.java)
+            startActivity(intent) }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    KotlinArtisanTheme {
+        Greeting("Android")
+    }
+}
