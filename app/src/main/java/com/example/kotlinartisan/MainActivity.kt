daf5173142eef36b09d95ac2f637f043696e5b11
KMarly D <44208106+Kemarly@users.noreply.github.com>
2023-11-21 13:03:41 -0500

    --------- beginning of main 2023-11-21 09:25:03.500 16374-16374 studio.deploy           pid-16374                            E  Could not get package user id: run-as: unknown package: com.example.kotlinartisan 2023-11-21 09:25:03.526 16374-16374 studio.deploy           pid-16374                            E  Could not find apks for this package: com.example.kotlinartisan 2023-11-21 09:25:03.547 16374-16374 studio.deploy           pid-16374                            E  Could not get package user id: run-as: unknown package: com.example.kotlinartisan 2023-11-21 09:25:03.560 16374-16374 studio.deploy           pid-16374                            E  Could not find apks for this package: com.example.kotlinartisan 2023-11-21 09:25:05.416   615-668   VerityUtils             system_process                       E  Failed to measure fs-verity, errno 1: /data/app/~~vUEw6jiywCUCYFbzTItKdQ==/com.example.kotlinartisan-glXmo4tdBNZS2Fh8d5IFuQ==/base.apk 2023-11-21 09:28:30.940   615-668   VerityUtils             system_process                       E  Failed to measure fs-verity, errno 1: /data/app/~~e2K1lEDv4zdJIked7mgq-w==/com.example.kotlinartisan-10qf4Qc9rw-UuhnmLZgP9g==/base.apk 2023-11-21 09:28:31.361   615-668   VerityUtils             system_process                       E  Failed to measure fs-verity, errno 1: /data/app/~~e2K1lEDv4zdJIked7mgq-w==/com.example.kotlinartisan-10qf4Qc9rw-UuhnmLZgP9g==/base.apk 2023-11-21 09:35:55.335   615-668   VerityUtils             system_process                       E  Failed to measure fs-verity, errno 1: /data/app/~~Vlr6lyFIZ07yRwo01OQz_w==/com.example.kotlinartisan-EOXtO4ipXpH-QioeM6V-cg==/base.apk

--- app/src/main/java/com/example/kotlinartisan/MainActivity.kt
+++ app/src/main/java/com/example/kotlinartisan/MainActivity.kt
@@ -4,10 +4,6 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.ImageButton
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
